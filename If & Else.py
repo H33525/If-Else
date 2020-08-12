cb5d9af64a62ca6bac37f79d32bb20c9f83c3c0b
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-08-12 12:32:11 +0530

    Add files via upload

---
+++ If & Else.py
@@ -0,0 +1,14 @@
+is_old= False
+is_licenced= False
+
+if is_old:
+   print('You are old enough to drive!')
+
+elif:
+   print('You can drive now!')
+
+else:
+
+  print('You are not of age!')
+
+print('koko')
